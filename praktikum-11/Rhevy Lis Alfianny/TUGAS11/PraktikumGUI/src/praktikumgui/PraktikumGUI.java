7712342e6962d9acf26e9b12240ff835ad59e7d3
RhevyLisAlfianny <rhevylis48@gmail.com>
2019-11-06 18:51:36 +0700

    update rhevy

---
+++ praktikum-11/Rhevy Lis Alfianny/TUGAS11/PraktikumGUI/src/praktikumgui/PraktikumGUI.java
@@ -0,0 +1,14 @@
+
+package praktikumgui;
+
+
+public class PraktikumGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
